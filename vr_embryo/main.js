0ba0514423f86624fbed3886df5412b936ea53bd
sam <samface@gmail.com>
2017-11-29 15:35:51 -0500

    added axes

--- vr_embryo/main.js
+++ vr_embryo/main.js
@@ -1,41 +1,92 @@
-AFRAME.registerComponent("add-images", {
+function make(what) {
+  var el = document.createElement(what)
+  return el;
+}
+
+// AFRAME.registerComponent("axes", {
+//   init: function() {
+//     var entity = this.el
+//         _(3).times(axis => {
+//           _(20).times(n =>{
+//             var loc = n - 10
+//             var point = make("asphere")
+//             point.setAttribute("position", {
+//               x: (axis = 0) ? loc : 0,
+//               y: (axis = 1) ? loc : 0,
+//               z: (axis = 2) ? loc : 0
+//             })
+//             point.setAttribute("material", {
+//               color: "white"
+//             })
+//             point.setAttribute("light", {
+//               color: "white",
+//               intensity: 0.75
+//             })
+//             entity.appendChild(point)
+//           })
+//         })
+//   }
+// })
+
+AFRAME.registerComponent("axes", {
   init: function() {
-    console.log("happening")
-    var parent = document.querySelector("#mainscene")
-    console.log(parent)
+    var entity = this.el
+    var colors = ["rgb(255,0,0)", "rgb(0,255,100)", "rgb(0,0,255)"]
 
-    _(18).times(function(n) {
-      console.log("/assets/droso_WT/Mem_02/t_1_z_"+ (n+1) +".png")
+    var group = document.createElement("a-entity")
+    group.setAttribute("id", "axes")
 
-      var el = document.createElement("a-image")
-      el.setAttribute('material', {transparent: true, opacity: 0.5});
-      el.setAttribute('position', {x: 1, y: 2, z: n/18/2*-5});
-      el.setAttribute('src', '/assets/droso_WT/Mem_02/t_1_z_'+ n +'.png')
+    _(3).times(a => {
+      var axis = a
 
+      _(20).times(n =>{
+        var pos = n - 10
+        var loc = {
+          x: (axis == 0) ? pos : 0,
+          y: (axis == 1) ? pos : 0,
+          z: (axis == 2) ? pos : 0
+        }
+        // console.log("axis:", axis, "loc:", loc)
 
-      parent.appendChild(el)
+        var point = make("a-sphere")
+        point.setAttribute("radius", 0.05)
+        point.setAttribute("position", loc)
+        point.setAttribute("material", {
+          color: colors[axis],
+          transparent: true,
+          opacity: 0.75
+        })
+
+        var label = make("a-text")
+        label.setAttribute("value", pos)
+        label.setAttribute("position", loc)
+        label.setAttribute("color", colors[axis])
+
+        group.appendChild(point)
+        group.appendChild(label)
+      })
     })
+
+    entity.appendChild(group)
   }
 })
 
 
-AFRAME.registerComponent("image_init", {
-  schema: {
-    num: {type: 'number', default: 0}
-  },
+AFRAME.registerComponent("add-images", {
   init: function() {
-    var cmp = this.el
-    cmp.setAttribute("src", "/assets/droso_WT/Mem_02/t_1_z_"+ (this.data.num) +".png")
-    // cmp.setAttribute("position", {x: 1, y: 0, z: (this.data.num*.1)})
-    // cmp.setAttribute("material", "opacity", 0.15)
-    cmp.setAttribute("position", "1 0 " + (this.data.num*.1))
-    cmp.setAttribute("material", "opacity", "0.15")
+    var parent = document.querySelector("#embryo1")
+    console.log(parent)
 
-    console.log(cmp)
+    _(18).times(function(n) {
+      // console.log("/assets/droso_WT/Mem_02/t_1_z_"+ (n+1) +".png")
 
-  },
+      var el = document.createElement("a-image")
+      el.setAttribute('material', {transparent: true, opacity: 0.5});
+      el.setAttribute('position', {x: 1, y: 2, z: n/18/2*-5});
+      el.setAttribute('src', '/assets/droso_WT/Mem_02/t_1_z_'+ (n+1) +'.png')
 
-  update: function() {
 
+      parent.appendChild(el)
+    })
   }
 })

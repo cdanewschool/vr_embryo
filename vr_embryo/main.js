075d0c28b976b88a8af76c698fbf3d311bdb4659
sam <samface@gmail.com>
2017-11-30 16:54:47 -0500

    fixed livereloading, drawing images from embryo object

--- vr_embryo/main.js
+++ vr_embryo/main.js
@@ -1,19 +1,34 @@
+require('aframe');
+var _ = require('lodash')
+
 var app = app || {}
+
+app.ctl = {
+  spacing: 0.2,
+  initialposition: {x: 0, y: 0, z: -2}
+}
 
 // data model for the embryo
 app.embryo = {
   species: "drosophilia melanogaster", // name of the species
   locator: "dro-mel-fr-sl-2-450", // format: genus_species_type_lab_channels_frames
   slices: 18,
-  steps: 24,
+  steps: 25,
   time: 35280, // total seconds of recording
   frames: 450,
   framerate: 0, // will be useful if we want to display an image at a given time in seconds
   type: "fluorescence recording", // microscopy type
   origin: "Shvartsman Lab", // lab origin
   channels: {
-    "membrane staining": [],
-    "nuclear staining": []
+    // ideally these (and this whole object) would be generated server-side from the file hierarchy
+    "membrane-staining": {
+      path: "",
+      images: []
+    },
+    "nuclear-staining": {
+      path: "",
+      images: []
+    }
   },
   init: function() {
     var cmp = this
@@ -21,17 +36,30 @@ app.embryo = {
     // then after loading the images, call document.dispatchEvent('imagesloaded')
 
     cmp.framerate = cmp.frames/cmp.time
+    // calculate FPS of the acquisition
 
-    _(cmp.frames).times(f => {
-      _(cmp.channels).each((arr, key) => {
-        var folder = key.replace(/\s/, '-')
+    // generate paths for all filenames in all channels:
 
-        // iterate over each file
-        // var path = "assets/"+ cmp.locator +"/"+ folder +"/"+ filename
+    // for each channel:
+    _(cmp.channels).each((ch, key) => {
+      ch.path = "assets/datasets/" + cmp.locator + "/" + key
+      // for every step
+      _(cmp.steps).times(f => {
+        // and for every slice
+        _(cmp.slices).times(s => {
+          // console.log("channel", key, "frame", f, "slice", s)
+
+          // compose the filename
+          // e.g. assets/datasets/dro-mel-fr-sl-2-450/membrane-staining/t_24_z_17.png
+
+          var filename = "t_" + f + "_z_" + s + ".png"
+          var path = ch.path + "/" + filename
+
+          // and add it to this channel's "images" array
+          ch.images.push(path)
+        })
       })
     })
-
-
   },
   getImage: function(channel, time) {
     // an example of building functionality into the embryo object
@@ -94,21 +122,34 @@ AFRAME.registerComponent("axes", {
 })
 
 
-AFRAME.registerComponent("add-images", {
+AFRAME.registerComponent("embryo-stack", {
+  schema: {
+    accordion: {type: "number", default: app.ctl.spacing} // i *think* this is how you modify spacing on the fly?
+  },
   init: function() {
+    app.embryo.init()
+    console.log("embryo", app.embryo)
+  },
+  update: function() {
     var parent = document.querySelector("#embryo1")
-    console.log(parent)
 
-    _(18).times(function(n) {
-      // console.log("/assets/droso_WT/Mem_02/t_1_z_"+ (n+1) +".png")
+    _(app.embryo.channels).each((ch, channelname) => {
+      _(app.embryo.slices).times(function(n) {
+        // iterate through slices, "n" is number of current slice
+        // console.log("/assets/droso_WT/Mem_02/t_1_z_"+ (n) +".png")
 
-      var el = document.createElement("a-image")
-      el.setAttribute('material', {transparent: true, opacity: 0.5});
-      el.setAttribute('position', {x: 1, y: 2, z: n/18/2*-5});
-      el.setAttribute('src', '/assets/droso_WT/Mem_02/t_1_z_'+ (n+1) +'.png')
+        var el = document.createElement("a-image")
+        el.setAttribute('material', {
+          shader: "standard",
+          side: "double",
+          transparent: true,
+          opacity: 0.2,
+          src: ch.images[n]
+        });
+        el.setAttribute('position', {x: app.ctl.initialposition.x, y: app.ctl.initialposition.y, z: n/app.embryo.slices * app.ctl.spacing + app.ctl.initialposition.z});
 
-
-      parent.appendChild(el)
+        parent.appendChild(el)
+      })
     })
   }
 })
